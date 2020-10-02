f4ff8ee0c17383faabadb3a5d4136098ffc1f1ab
Ariful Alam <alamariful1727@gmail.com>
2020-10-02 17:08:49 +0600

    User's location detected done.

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
-  return <div>Event Locator Client</div>;
+  const [position, setPosition] = useState<{ latitude: number; longitude: number; accuracy: number }>();
+
+  const getPositions = (position: Position) => {
+    setPosition({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+      accuracy: position.coords.accuracy,
+    });
+  };
+
+  const handleLocationError = (error: PositionError) => {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        alert('User denied the request for Geolocation.');
+        break;
+      case error.POSITION_UNAVAILABLE:
+        alert('Location information is unavailable.');
+        break;
+      case error.TIMEOUT:
+        alert('The request to get user location timed out.');
+        break;
+      default:
+        alert(`A Geolocation error[${error.code}] occurred.`);
+        break;
+    }
+  };
+
+  const getLocation = React.useCallback(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(getPositions, handleLocationError, {
+        enableHighAccuracy: true,
+        timeout: 5000, // ? integer (milliseconds) - amount of time before the error callback is invoked, if 0 it will never invoke.
+        maximumAge: 0, // ? integer (milliseconds) | infinity - maximum cached position age.
+      });
+    } else {
+      alert('Geolocation is not supported by this browser.');
+    }
+  }, []);
+
+  useEffect(() => {
+    getLocation();
+  }, [getLocation]);
+
+  return (
+    <div>
+      <h1>Event Locator Client</h1>
+      {position && position.latitude && position.longitude && position.accuracy && (
+        <div>
+          <h2>Details</h2>
+          <h3>Latitude: {position.latitude}</h3>
+          <h3>Longitude: {position.longitude}</h3>
+          <h3>Accuracy: {position.accuracy}</h3>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Home;
