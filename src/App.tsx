4c5523a9c2e2baada87d5966aee9e292eaf89689
Ariful Alam <alamariful1727@gmail.com>
2020-10-02 15:25:43 +0600

    Router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react'
-
+import { BrowserRouter, Route, Switch, useLocation } from 'react-router-dom';
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
+      <Switch>
+      <Route exact path="/" render={() => <div>
       Event Locator Client
-    </div>
+    </div>} />
+    <Route path="*">
+            <NoMatch />
+          </Route>
+      </Switch>
+    </BrowserRouter>
   )
+}
+
+const NoMatch = () => {
+  let location = useLocation();
+
+  return (
+    <div>
+      <h3>
+        No match for <code>{location.pathname}</code>
+      </h3>
+    </div>
+  );
 }
 
 export default App
