0b0e2fcebfa9536e4e242165e7868f10c7eb62ef
Ariful Alam <alamariful1727@gmail.com>
2020-10-04 19:47:42 +0600

    anchor removed

--- src/Home.tsx
+++ src/Home.tsx
@@ -118,7 +118,6 @@ const Home = () => {
               icon={{
                 url: '/location.svg',
                 origin: new window.google.maps.Point(0, 0),
-                anchor: new window.google.maps.Point(25, 25),
                 scaledSize: new window.google.maps.Size(50, 50),
               }}
             />
