a1a2c6e8f0f7e9da2761c85a9d2b31e600ef0e9b
Ariful Alam <alamariful1727@gmail.com>
2020-10-04 19:30:12 +0600

    Find Me : feature added

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,37 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { GoogleMap, useLoadScript } from '@react-google-maps/api';
+import React, { useState } from 'react';
+import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api';
 import { MAP_KEY } from './config';
 
-const mapContainerStyle = {
-  height: '100%',
-  width: '100%',
-};
-const options = {
-  // disableDefaultUI: true,
-  // zoomControl: true,
-};
 const center = {
   lat: 23.765057,
   lng: 90.388661,
 };
 
+interface ICurrentPosition {
+  latitude: number;
+  longitude: number;
+  accuracy: number;
+  show: boolean;
+}
+
 const Home = () => {
-  const [position, setPosition] = useState<{ latitude: number; longitude: number; accuracy: number }>();
+  const [currentPosition, setCurrentPosition] = useState<ICurrentPosition>();
 
   // @react-google-maps/api states
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: MAP_KEY,
   });
-  const [zoom] = useState(13);
-
-  const getPositions = (position: Position) => {
-    console.log(position);
-    setPosition({
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude,
-      accuracy: position.coords.accuracy,
-    });
-  };
+  const [zoom] = useState(14);
+  const mapRef = React.useRef<google.maps.Map<Element>>();
 
   const handleLocationError = (error: PositionError) => {
     switch (error.code) {
@@ -50,39 +41,79 @@ const Home = () => {
     }
   };
 
-  const getLocation = React.useCallback(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(getPositions, handleLocationError, {
-        enableHighAccuracy: true,
-        timeout: 5000, // ? integer (milliseconds) - amount of time before the error callback is invoked, if 0 it will never invoke.
-        maximumAge: 0, // ? integer (milliseconds) | infinity - maximum cached position age.
-      });
-    } else {
-      alert('Geolocation is not supported by this browser.');
+  const onMapLoad = React.useCallback((map: google.maps.Map<Element>) => {
+    mapRef.current = map;
+  }, []);
+
+  const panTo = React.useCallback(({ lat, lng }) => {
+    if (mapRef && mapRef.current) {
+      mapRef.current.panTo({ lat, lng });
+      mapRef.current.setZoom(14);
     }
   }, []);
 
-  useEffect(() => {
-    getLocation();
-  }, [getLocation]);
+  const getLocation = React.useCallback(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position: Position) => {
+          setCurrentPosition({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            accuracy: position.coords.accuracy,
+            show: true,
+          });
+          panTo({ lat: position.coords.latitude, lng: position.coords.longitude });
+        },
+        handleLocationError,
+        {
+          enableHighAccuracy: true,
+          timeout: 5000, // ? integer (milliseconds) - amount of time before the error callback is invoked, if 0 it will never invoke.
+          maximumAge: 0, // ? integer (milliseconds) | infinity - maximum cached position age.
+        },
+      );
+    } else {
+      alert('Geolocation is not supported by this browser.');
+    }
+  }, [panTo]);
 
   if (loadError) return <p>{loadError}</p>;
   if (!isLoaded) return <p>Loading...</p>;
 
   return (
-    <div>
-      <h1>Event Locator Client</h1>
-      <p>For nearby feature, you have to give permission for your location.</p>
-      {position && (
-        <div>
-          <h2>Your location</h2>
-          <p>Latitude: {position.latitude}</p>
-          <p>Longitude: {position.longitude}</p>
-          <p>Accuracy: {position.accuracy}</p>
-        </div>
-      )}
-      <div style={{ height: '70vh' }}>
-        <GoogleMap mapContainerStyle={mapContainerStyle} zoom={zoom} center={center} options={options}></GoogleMap>
+    <div className="antialiased h-screen container mx-auto px-3 py-4 md:flex md:space-x-3">
+      <div className="md:w-64">
+        <h1 className="font-semibold text-2xl">Event Locator Client</h1>
+        <p>For nearby feature, you have to give permission for your location.</p>
+        <button
+          className="bg-gray-400 px-2 py-1 my-2 rounded text-white font-semibold hover:bg-gray-500 transition ease-in-out duration-300 focus:outline-none"
+          onClick={getLocation}
+        >
+          Find Me
+        </button>
+        {currentPosition && currentPosition.show && (
+          <div className="my-5">
+            <h2 className="font-semibold">Your location</h2>
+            <p>Latitude: {currentPosition.latitude}</p>
+            <p>Longitude: {currentPosition.longitude}</p>
+            <p>Accuracy: {currentPosition.accuracy}</p>
+          </div>
+        )}
+      </div>
+      <div className="h-full md:flex-1">
+        <GoogleMap mapContainerClassName="h-full" zoom={zoom} center={center} onLoad={onMapLoad}>
+          {currentPosition && currentPosition.show && (
+            <Marker
+              key={currentPosition.accuracy}
+              position={{ lat: currentPosition.latitude, lng: currentPosition.longitude }}
+              icon={{
+                url: '/location.svg',
+                origin: new window.google.maps.Point(0, 0),
+                anchor: new window.google.maps.Point(15, 15),
+                scaledSize: new window.google.maps.Size(30, 30),
+              }}
+            />
+          )}
+        </GoogleMap>
       </div>
     </div>
   );
