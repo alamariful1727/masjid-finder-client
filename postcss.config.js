b30fbe285743399d5c7e602b56962f9f931ba004
Ariful Alam <alamariful1727@gmail.com>
2020-10-12 09:49:01 +0600

    optimize production build

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  plugins: [require('tailwindcss')('./tailwind.js'), require('autoprefixer')],
+  plugins: [
+    require('tailwindcss')('./tailwind.js'),
+    require('autoprefixer'),
+    process.env.NODE_ENV === 'production' &&
+      require('@fullhuman/postcss-purgecss')({
+        content: [
+          './src/**/*.html',
+          './src/**/*.tsx',
+          './src/**/*.ts',
+          './src/**/*.jsx',
+          './src/**/*.js',
+          './public/index.html',
+        ],
+        defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+      }),
+  ],
 };
